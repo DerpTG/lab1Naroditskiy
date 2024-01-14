7b1bcf896c919e3fd4cbaf7a68b2a004d26c25cc
DerpTG <fnaroditskiy@gmail.com>
2024-01-13 20:33:29 -0500

    Terminal Commit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String args[]){
         System.out.println("hiii");
         System.out.println("242 is cool");
+        System.out.println("better than 210");
     }
 }
