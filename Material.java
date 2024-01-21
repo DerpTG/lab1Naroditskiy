1bba43f44f028c1ce2cd4caba558e94adbf68af7
DerpTG <fnaroditskiy@gmail.com>
2024-01-20 21:25:04 -0500

    Headers commit

--- Material.java
+++ Material.java
@@ -1,3 +1,12 @@
+/** Project: Solo Lab 1 Motorbike
+ * Purpose Details: Demonstrate Classes and Proficient Use of GitHub
+ * Course: IST 242
+ * Author: Felix Naroditskiy
+ * Date Developed: 1/17/2024
+ * Last Date Changed: 1/20/2024
+ * Rev: 1.0
+ */
+
 /**
  * Enum representing various materials used in manufacturing motorbike frames.
  */
